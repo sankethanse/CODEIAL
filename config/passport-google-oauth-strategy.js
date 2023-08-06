06ab912cb3cab3b57520792a6868fbbb6685174f
Sanket Hanse <sankethanse@gmail.com>
2023-08-06 19:12:26 +0530

    google oauth strategy

---
+++ config/passport-google-oauth-strategy.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+// tell passport to use new stategy for googlr login
+passport.use(new googleStrategy({
+    clientID : "763123930748-4tg40ookv0e96bprlc0r2ce7v68a3isd.apps.googleusercontent.com",
+    clientSecret : "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",
+    callbackURL : "http://localhost:8000/users/auth/google/callback"  
+},
+   async function(accessToken , refreshToken,profile,done){
+    //find user
+         const user = await User.findOne({email : profile.emails[0].value}).exec()
+            if(err){
+                console.log('error in google stategy pasport ', err);
+            }
+            if(user){
+                // if found,  set this user as req.user
+                return done(null, user);
+            }
+            else{
+                // if not found create the user and set it as req.user
+                User.create({
+                    name : profile.displayName,
+                    email : profile.emails[0].value,
+                    password : crypto.randomBytes(20).toString('hex')
+                } , function(err,user){
+                    if(err){
+                        console.log('error in google stategy pasport ', err);
+                    }
+                    return done(null,user);
+                })
+            }
+    }
+))
+
+
+module.exports = passport;
